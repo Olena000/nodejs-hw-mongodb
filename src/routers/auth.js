27bb011fd1a36070e6b1d6ff85ecd0427d3fab2b
Olena000 <154604010+Olena000@users.noreply.github.com>
2024-10-08 16:39:36 +0300

    login

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,6 +3,8 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { registerUserSchema } from '../validation/auth.js';
 import { registerUserController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { loginUserSchema } from '../validation/auth.js';
+import { loginUserController } from '../controllers/auth.js';
 
 const authRouter = Router();
 
@@ -10,6 +12,12 @@ authRouter.post(
   '/auth/register',
   validateBody(registerUserSchema),
   ctrlWrapper(registerUserController),
+);
+
+authRouter.post(
+  '/auth/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController),
 );
 
 export default authRouter;
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import { env } from './utils/env.js';
 import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -14,6 +15,8 @@ export default function setupServer() {
   app.use(express.json());
 
   app.use(cors());
+
+  app.use(cookieParser());
 
   app.use(
     pino({
